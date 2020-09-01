80cc2aa19cfa61ebe32df5ad09be0ad749ae3ce0
finlaywashere <finman292004@protonmail.com>
2020-09-01 19:29:13 -0400

    Removed more binaries from git

--- libc/src/mem.d
+++
@@ -1,4 +0,0 @@
-src/mem.o: src/mem.c include/libc/mem.h \
- /usr/lib/gcc/x86_64-elf/9.2.0/include/stdint.h \
- /usr/lib/gcc/x86_64-elf/9.2.0/include/stdint-gcc.h \
- /usr/lib/gcc/x86_64-elf/9.2.0/include/stddef.h
--- libc/src/syscall.d
+++
@@ -1,4 +0,0 @@
-src/syscall.o: src/syscall.c include/libc/syscall.h \
- /usr/lib/gcc/x86_64-elf/9.2.0/include/stdint.h \
- /usr/lib/gcc/x86_64-elf/9.2.0/include/stdint-gcc.h \
- /usr/lib/gcc/x86_64-elf/9.2.0/include/stddef.h include/libc/string.h
