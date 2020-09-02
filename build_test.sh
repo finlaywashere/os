c148f0a5108d047d22e679afa8bb58a7ff30f978
finlaywashere <finman292004@protonmail.com>
2020-09-02 12:30:05 -0400

    Made test code update in the iso on build because it was a pain to do it manually and added a leading line ending to the test string

--- build_test.sh
+++ build_test.sh
@@ -4,3 +4,9 @@ rm test.o test.bin
 nasm -f elf64 test.S -o test.o
 x86_64-elf-ld test.o libc/libc.a -o test.bin
 
+mkdir disk_mnt
+echfs-fuse disk.iso disk_mnt
+rm disk_mnt/test.bin
+cp test.bin disk_mnt/test.bin
+umount disk_mnt
+
