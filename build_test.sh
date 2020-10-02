5ee8f410f9c15e7e265faf19d6a3f6177ca30658
finlaywashere <finman292004@protonmail.com>
2020-10-02 16:39:20 -0400

    Added linker script

--- build_test.sh
+++ build_test.sh
@@ -3,7 +3,7 @@ rm test.o test.bin
 
 #nasm -f elf64 test.S -o test.o
 x86_64-elf-gcc --sysroot=/home/finlay/active/programming/C/OS/sysroot -isystem=/usr/include -MD -c test.c -o test.o -std=gnu11 -O2 -g -ffreestanding -Wextra -Wall -mno-red-zone
-x86_64-elf-ld test.o libc/libc.a -o test.bin
+x86_64-elf-ld -T test_linker.ld test.o libc/libc.a -o test.bin
 
 mkdir disk_mnt
 echfs-fuse disk.iso disk_mnt
